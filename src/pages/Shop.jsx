d79caefff02788bc24f68a8fdd014671bc52d66a
salihsungurcelik <salihcelik099@gmail.com>
2024-09-08 20:19:50 +0300

    devam

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,9 +1,11 @@
+import Hero from "../components/Hero/Hero";
+
 const Shop = () => {
   return (
     <div>
-        
+      <Hero />
     </div>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
